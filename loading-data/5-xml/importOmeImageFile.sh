0ebe89f1a28db1578d0f0e1a810018ad62d690d1
jonathan.janos@mongodb.com <jonathan.janos@mongodb.com>
2021-11-01 14:22:22 -0400

    Added XML example

---
+++ loading-data/5-xml/importOmeImageFile.sh
@@ -0,0 +1,2 @@
+mongoimport mongodb://localhost:27017 --db=DEMO --collection=ome --file=single-image.ome.json
+
---
+++ loading-data/5-xml/importOmeROIFile.sh
@@ -0,0 +1,2 @@
+mongoimport mongodb://localhost:27017 --db=DEMO --collection=ome --file=ROI.ome.json
+

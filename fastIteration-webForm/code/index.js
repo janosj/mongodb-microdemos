16698123bea0fa0d885e66b1257adbfaaeebf2c2
jonathan.janos@mongodb.com <jonathan.janos@mongodb.com>
2022-04-01 11:56:26 -0400

    support for schema validation

--- fastIteration-webForm/code/index.js
+++ fastIteration-webForm/code/index.js
@@ -1,3 +1,4 @@
+const { resolveCaa } = require('dns');
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -15,6 +16,16 @@ const port = 8081
 // WARNING: using localhost here causes an error:
 const uri = `mongodb://myserver1:27017`;
 
+// Establish MDB connection on startup.
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    if(err){
+        console.log(err);
+        reject(err);
+    }
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/web/index.html'));
 })
@@ -24,27 +35,36 @@ app.use(express.json());
 app.post('/insertDocument', (req, res) => {
     insertDocument(req.body).then((result) => {
         res.send(JSON.stringify(result));
+    // }).catch(error => console.error(error))
+    }).catch((error) => {
+        // Send the error back to the browser.
+        res.status(500).send(JSON.stringify(error));
     })
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}! Access from browser at http://localhost:${port}/`))
 
 function insertDocument(doc) {
+
     return new Promise(function (resolve, reject) {
 
-        const MongoClient = require('mongodb').MongoClient;
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        client.connect(err => {
-            if(err){
-                console.log(err);
-                reject(err);
-            }
-            const collection = client.db("DEMO").collection("workflow");
-            collection.insertOne(doc).then((result) => {
-                resolve(result);
-            });
+        //const collection = client.db("DEMO").collection("workflow");
+        //collection.insertOne(doc).then((result) => {
+        //    resolve(result);
+        //});
 
+        // Updated to propagate errors.
+        const collection = client.db("DEMO").collection("workflow");
+        collection.insertOne(doc, (err, res) => {
+            if (err) {
+                console.log('error', err);
+                reject(err);
+            } else {
+                console.log('\ninserted record', res.ops[0]);
+                resolve(res.ops[0]);
+            }
         });
-    })
+
+    });
 
 }

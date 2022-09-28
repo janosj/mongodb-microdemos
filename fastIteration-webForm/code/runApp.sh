01cd8d89940cf1e62fb42e456b29c12566ceb08f
jonathan.janos@mongodb.com <jonathan.janos@mongodb.com>
2022-09-28 16:49:24 -0400

    cleanup

--- fastIteration-webForm/code/runApp.sh
+++ fastIteration-webForm/code/runApp.sh
@@ -7,7 +7,7 @@
 # Atlas Commercial
 # MDB_URI=mongodb+srv://<user>:<password>@your-atlas-cluster.mongodb.net/DEMO?retryWrites=true&w=majority
 
-# Kubernetes
+# Kubernetes Example
 # MDB_URI=mongodb://mdb-rs-0.devsecops.mongodb.svc.cluster.local:27017,mdb-rs-1.devsecops.mongodb.svc.cluster.local:27017,mdb-rs-2.devsecops.mongodb.svc.cluster.local:27017/?replicaSet=mdb-rs;
 
 URI=$MDB_URI node index.js

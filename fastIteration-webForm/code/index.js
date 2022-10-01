ed38b53a6ef98dcb00455d55a707ee519b6c0ceb
jonathan.janos@mongodb.com <jonathan.janos@mongodb.com>
2022-10-01 11:55:10 -0400

    Corrected async connection issue

--- fastIteration-webForm/code/index.js
+++ fastIteration-webForm/code/index.js
@@ -4,7 +4,7 @@ const { exit } = require('process');
 const app = express()
 
 // Reconfigured logging to write to console + file.
-// File logging is useful for troubleshooting with Kubernetes.
+// Introduced to support troubleshooting on Kubernetes.
 var fs = require('fs');
 var util = require('util');
 var log_file = fs.createWriteStream('debug/debug.log', {flags : 'w'});
@@ -14,7 +14,10 @@ console.log = function(d) {
     log_stdout.write(util.format(d) + '\n');
 };
 
+// Pick up the MongoDB URI as an environment variable.
 uri = process.env.URI;
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 4000 });
 if (uri) {
     console.log(`Starting app with MONGODB URI = ${uri}`);
 } else {
@@ -26,54 +29,45 @@ if (uri) {
 // Note: uses 8081 to leave 8080 available for Ops Manager or anything else.
 const port = 8081
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/web/index.html'));
-})
 app.use(express.static('web'));
+
+// parses incoming json requests and puts parsed data into request.body.
 app.use(express.json());
 
-app.post('/insertDocument', (req, res) => {
-    insertDocument(req.body).then((result) => {
-        res.send(JSON.stringify(result));
-    // }).catch(error => console.error(error))
-    }).catch((error) => {
-        // Send the error back to the browser.
-        res.status(500).send(JSON.stringify(error));
-    })
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname + '/web/index.html'));
+})
+
+app.post('/insertDocument', async (request, response) => {
+
+//    const MongoClient = require('mongodb').MongoClient;
+//    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 4000 });
+    
+    client.connect().then(
+
+        function() {
+
+            console.log("\nEstablished database connection.");
+            const collection = client.db("DEMO").collection("workflow");
+
+            collection.insertOne(request.body).then( (result) => {
+                response.send(JSON.stringify(result));
+                console.log('Inserted document:');
+                console.log(result.ops[0]);
+            }).catch( error => { 
+                console.error("Error caught while inserting document:");  
+                console.error(error);
+                response.status(500).send(error);
+            });
+
+        }
+
+    ).catch( (error) => {
+        console.error("ERROR in /insertDocument while trying to connect to MongoDB.");
+        console.error(error);
+        response.status(500).send(error);
+    } )
+
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}! Access from browser at http://localhost:${port}/`))
-
-function insertDocument(doc) {
-
-    return new Promise(function (resolve, reject) {
-
-        // Returned MDB connection to inside insertDocument.
-        // Had moved it previously to connect on startup.
-        // Putting it here allows connections to be re-established as needed.
-        const MongoClient = require('mongodb').MongoClient;
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        client.connect(err => {
-            if(err){
-                console.log(`ERROR connecting to MongoDB! MDB_URI is: ${uri}`);
-                console.log(err);
-                reject(err);
-            }
-        });
-
-        // Updated to propagate errors.
-        const collection = client.db("DEMO").collection("workflow");
-        collection.insertOne(doc, (err, res) => {
-            if (err) {
-                console.log(err);
-                reject(err);
-            } else {
-                console.log('\ninserted record');
-                console.log(res.ops[0]);
-                resolve(res.ops[0]);
-            }
-        });
-
-    });
-
-}
